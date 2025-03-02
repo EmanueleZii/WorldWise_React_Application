eaca998e583cb16d6b3b2ca600786f7f304400ed
unknown <zignale92@gmail.com>
2025-03-02 17:51:11 +0100

    map points blues

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,9 +3,13 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
  function Map() {
   const navigate = useNavigate();
+
+  const {cities} = useCities();
+
   const [mapPosition, setMapPosition] = useState([40,53]);
 
   const [searchParams, setSearchParams] = useSearchParams()
@@ -20,11 +24,13 @@ import { useState } from 'react';
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
+       {cities.map((city) =>  <Marker position={[city.position.lat,city.position.lng]} 
+       key={city.id}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+          <span>{city.emoji}</span>
+          <span>{city.cityName}</span>
           </Popup>
-        </Marker>
+        </Marker> ) }
       </MapContainer>
     </div>
   )
