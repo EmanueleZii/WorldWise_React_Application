16770bb94999133d3fe634ee76972aa9c113de30
unknown <zignale92@gmail.com>
2025-03-05 10:53:21 +0100

    application completete

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,15 +1,17 @@
-import SideBar from '../components/SideBar';
-import styles from './AppLayout.module.css';
-import Map from '../components/Map';
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import User from "../components/User";
+import styles from "./AppLayout.module.css";
+//import ProtectedRoute from "./ProtectedRoute";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
- 
-      <SideBar />
-      <Map />
+        <Sidebar />
+        <Map />
+        <User />
     </div>
-  )
+  );
 }
 
 export default AppLayout;
